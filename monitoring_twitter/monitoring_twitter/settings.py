92a3d38602a43f158b8637f25f60cf30f617aa8f
Fabiano Teichmann <fabiano.geek@gmail.com>
2020-02-17 14:20:54 -0300

    Configuração Heroku

--- monitoring_twitter/monitoring_twitter/settings.py
+++ monitoring_twitter/monitoring_twitter/settings.py
@@ -126,7 +126,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MESSAGE_LEVEL = 10
-django_heroku.settings(locals())
